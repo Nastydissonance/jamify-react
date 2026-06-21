99ccac4ca0000393a5f1e4b73faf52a8141dd869
HorseNoremorse <nastydissonance@gmail.com>
2026-06-21 14:42:53 +0300

    feat: add login page and protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import { lazy, Suspense } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom'; // ← BrowserRouter убран
+import ProtectedRoute from './components/ProtectedRoute';
+import LoginPage from './pages/LoginPage/LoginPage';
 
-const HomePage = lazy(() => import("./pages/HomePage"))
-const MusicianPage = lazy(() => import("./pages/MusicianPage"))
+const HomePage = lazy(() => import('./pages/HomePage'));
+const MusicianPage = lazy(() => import('./pages/MusicianPage'));
 
 function App() {
   return (
     <Suspense fallback={<div className="loader">Loading...</div>}>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/musician/:id" element={<MusicianPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/musician/:id"
+          element={
+            <ProtectedRoute>
+              <MusicianPage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Suspense>
   );
 }
 
-export default App
+export default App;
